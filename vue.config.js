a0b340947fcba286c76200d189fe8d7f1e3d6674
刘孝林 <13106917538@163.com>
2022-05-11 09:44:21 +0800

    删除多余的文件，保留基本的

--- vue.config.js
+++ vue.config.js
@@ -8,22 +8,10 @@ function resolve(dir) {
 
 const name = defaultSettings.title || 'vue Admin Template' // page title
 
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following methods:
-// port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
   publicPath: '/',
   outputDir: 'dist',
   assetsDir: 'static',
@@ -36,11 +24,24 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    proxy: {
+      // 路由转发，重定向
+      // 如:localhost:3010 => www.baidu.com:3010
+      '/api': {
+        // target: 'www.baidu.com',
+        // 本地
+        target: 'http://www.itcbc.com:8000',
+        // 设置跨域
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        },
+        logLevel: 'debug'
+      }
+    },
+    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
     name: name,
     resolve: {
       alias: {
@@ -49,7 +50,6 @@ module.exports = {
     }
   },
   chainWebpack(config) {
-    // it can improve the speed of the first screen, it is recommended to turn on preload
     config.plugin('preload').tap(() => [
       {
         rel: 'preload',
