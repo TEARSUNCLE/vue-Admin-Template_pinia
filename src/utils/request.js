936d2d66a5270e5f9c9eb8980576d3f6cefd6dca
刘孝林 <13106917538@163.com>
2022-05-11 14:50:02 +0800

    封装请求拦截和响应拦截

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
-import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
-import { getToken } from '@/utils/auth'
-
+import router from '@/router'
+import { getTimeStamp } from '@/utils/auth'
+import { Message } from 'element-ui'
+const Timeout = 3600
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
@@ -10,76 +11,55 @@ const service = axios.create({
   timeout: 5000 // request timeout
 })
 
-// request interceptor
-service.interceptors.request.use(
-  config => {
-    // do something before request is sent
+function checkTimeOut() {
+  const dateNow = Date.now()
+  const authTime = getTimeStamp()
+  // 毫秒除以1000变成秒，要一个小时才生效
+  return (dateNow - authTime) / 1000 > Timeout
+}
 
-    if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+// 请求拦截
+service.interceptors.request.use(req => {
+  // 有token
+  if (store.getters.token) {
+    if (checkTimeOut()) {
+      store.dispatch('user/userLogout')
+      router.push('/login')
+      return Promise.reject(new Error('会话已过期，请重新登录'))
     }
-    return config
-  },
-  error => {
-    // do something with request error
-    console.log(error) // for debug
-    return Promise.reject(error)
+    req.headers.Authorization = `Bearer ${store.getters.token}`
   }
-)
+  return req
+}, error => {
+  return Promise.reject(error)
+})
 
-// response interceptor
-service.interceptors.response.use(
-  /**
-   * If you want to get http information such as headers or status
-   * Please return  response => response
-  */
-
-  /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
-   */
-  response => {
-    const res = response.data
-
-    // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 20000) {
-      Message({
-        message: res.message || 'Error',
-        type: 'error',
-        duration: 5 * 1000
-      })
-
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
-        MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-          confirmButtonText: 'Re-Login',
-          cancelButtonText: 'Cancel',
-          type: 'warning'
-        }).then(() => {
-          store.dispatch('user/resetToken').then(() => {
-            location.reload()
-          })
-        })
-      }
-      return Promise.reject(new Error(res.message || 'Error'))
-    } else {
-      return res
-    }
-  },
-  error => {
-    console.log('err' + error) // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    })
-    return Promise.reject(error)
+// 响应拦截
+service.interceptors.response.use((res) => {
+  if (res.status && (res.status === 200)) {
+    store.commit('user/setLoginTime')
+    return res.data
+  } else {
+    Message.error(res.statusText)
+    return Promise.reject(new Error(res.statusText))
   }
-)
+}, (err) => {
+  console.log(err.response)
+  // 后台对于不合法token的处理
+  if (err.response && err.response.data && err.response.data.statusCode === 403) {
+    store.dispatch('user/userLogout')
+    router.push('/login')
+    err.message = '违法的验证信息，请重新登录'
+  }
+  if (err.response && err.response.data && err.response.data.statusCode === 400) {
+    err.message = '请求失败'
+  }
+  // else {
+  //   err.message = '业务繁忙，请重试'
+  // }
+  // Message.error(err.message)
+  Message.error(err.message)
+  return Promise.reject(err)
+})
 
 export default service
