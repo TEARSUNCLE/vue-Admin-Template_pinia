32196a75e9d589081094f06e91bf8210726ed440
刘孝林 <13106917538@163.com>
2022-05-11 14:48:24 +0800

    修改全局导航守卫

--- src/permission.js
+++ src/permission.js
@@ -1,64 +1,29 @@
-import router from './router'
-import store from './store'
+import router from '@/router'
+import store from '@/store'
+import { getToken } from './utils/auth'
 import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
-
-const whiteList = ['/login'] // no redirect whitelist
-
+// 导航守卫
 router.beforeEach(async(to, from, next) => {
-  // start progress bar
-  NProgress.start()
+  const token = getToken()
+  const url = to.path
+  const whiteList = ['/login', '/404']
 
-  // set page title
-  document.title = getPageTitle(to.meta.title)
-
-  // determine whether the user has logged in
-  const hasToken = getToken()
-
-  if (hasToken) {
-    if (to.path === '/login') {
-      // if is logged in, redirect to the home page
-      next({ path: '/' })
-      NProgress.done()
-    } else {
-      const hasGetUserInfo = store.getters.name
-      if (hasGetUserInfo) {
-        next()
-      } else {
-        try {
-          // get user info
-          await store.dispatch('user/getInfo')
-
-          next()
-        } catch (error) {
-          // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
-        }
-      }
-    }
+  // 登录了去其他页面
+  if (!whiteList.includes(url) && token) {
+    // 没有用户信息
+    // if (!store.state.user.userInfo.id) {
+    //   await store.dispatch('user/getUserInfo', store.state.user.userId)
+    // }
+    next()
+  } else if // 没登录 去的不是白名单
+  (!token && !whiteList.includes(url)) {
+    Message.error('请先登录')
+    next('/login')
+  } else if// 登陆了去登录页
+  (token && url === '/login') {
+    next('/')
   } else {
-    /* has no token*/
-
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
-      next()
-    } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
-    }
+    next()
   }
-})
-
-router.afterEach(() => {
-  // finish progress bar
-  NProgress.done()
 })
